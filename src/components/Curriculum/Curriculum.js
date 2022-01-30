6ac276899700d3ec2cb1f0eb050857a7952d8698
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-01-30 11:14:45 +0100

    SVG illustration added on Curriculum.js section

--- src/components/Curriculum/Curriculum.js
+++ src/components/Curriculum/Curriculum.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Typography, CardContent, Card } from "@mui/material";
+import {
+  Typography,
+  CardContent,
+  Card,
+  Box,
+  Icon,
+  SvgIcon,
+  Grid,
+} from "@mui/material";
+import curriculumSVG from "../../assets/curriculum.svg";
 import useStyles from "../Utils/Styles";
 
 const Curriculum = () => {
@@ -8,18 +17,32 @@ const Curriculum = () => {
     <Card className={classes.card} id="curriculum">
       <CardContent>
         <Typography gutterBottom variant="h4" align="center">
-          Il mio curriculum
+          Curriculum
         </Typography>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item xs={12} sm={6}>
+            <Box p={2} sx={{ display: "flex", justifyContent: "center" }}>
+              <img
+                style={{ width: "50vw" }}
+                src={curriculumSVG}
+                alt="uomo che guarda il curriculum"
+              ></img>
+            </Box>
+          </Grid>
 
-        <Typography p={2} align="justify" variant="subtitle1" gutterBottom>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-          laborum ab quod cum similique aspernatur odit debitis non laudantium.
-          Tenetur fuga facere expedita autem asperiores doloribus commodi, iure
-          sapiente cum. Lorem ipsum dolor sit amet consectetur, adipisicing
-          elit. Debitis consectetur rerum mollitia perferendis, esse laborum
-          vero? Mollitia enim explicabo et maiores dicta molestiae id fuga,
-          ducimus dignissimos! Corrupti, quidem dolores.
-        </Typography>
+          <Grid item xs={12} sm={6}>
+            <Typography p={2} align="justify" variant="subtitle1" gutterBottom>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Cupiditate laborum ab quod cum similique aspernatur odit debitis
+              non laudantium. Tenetur fuga facere expedita autem asperiores
+              doloribus commodi, iure sapiente cum. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Debitis consectetur rerum mollitia
+              perferendis, esse laborum vero? Mollitia enim explicabo et maiores
+              dicta molestiae id fuga, ducimus dignissimos! Corrupti, quidem
+              dolores.
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
