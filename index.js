66497fbc97e33bac1c5e51d90e666d72b6fb9de7
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-05-06 12:51:31 +0200

    added nodeenv production in package.json

--- index.js
+++ index.js
@@ -22,20 +22,22 @@ const whitelist = [
   "https://marinatricolidoc.herokuapp.com/",
 ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin);
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable");
+//       callback(null, true);
+//     } else {
+//       console.log("Origin rejected");
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // if (process.env.NODE_ENV === "production") {
 app.use(express.static(path.join(__dirname, "client/build")));
