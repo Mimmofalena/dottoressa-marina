8c191beaae5b9fdd918cb982e0f5421f6610e922
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-05-06 12:41:16 +0200

    added nodeenv production in package.json

--- index.js
+++ index.js
@@ -97,13 +97,13 @@ app.post(`/form`, async (req, res, next) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname), "./client/build", "index.html");
-  });
-}
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname), "./client/build", "index.html");
+});
+// }
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
