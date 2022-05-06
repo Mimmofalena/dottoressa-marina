d3d721e7fb05b39f524417a81122d122ef6845f4
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-05-06 11:27:49 +0200

    making server + client work together

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
 const app = express();
 require("dotenv").config();
@@ -7,35 +8,34 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-const PORT = process.env.PORT || 3002;
-app.use(cors());
+const PORT = process.env.PORT || 8080;
+app.use(helmet());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "http://www.dottoressamarinatricoli.it/"
-//   ); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+const whitelist = [
+  "http://localhost:3000/",
+  " http://localhost:8080/",
+  "https://marinatricolidoc.herokuapp.com/",
+];
 
-// process.env.NODE_ENV = "development";
-// process.env.NODE_ENV = "production";
-// let URL = "";
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// if (process.env.NODE_ENV === "production") {
-//   URL = process.env.URL_PRODUCTION;
-// } else if (process.env.NODE_ENV === "development") {
-//   URL = process.env.URL_DEVELOPMENT;
-// }
-
+app.use(cors(corsOptions));
 app.get(`/`, (req, res, next) => {
   try {
     res.json({
@@ -46,9 +46,13 @@ app.get(`/`, (req, res, next) => {
   }
 });
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname), "../client/build", "index.html");
+  });
+}
 
 app.post(`/form`, async (req, res, next) => {
   try {
