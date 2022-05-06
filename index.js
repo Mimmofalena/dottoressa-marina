7a181d9793dd3beeea90c571a1eb36a490322aeb
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-05-06 12:32:07 +0200

    modified path index.js to serve react

--- index.js
+++ index.js
@@ -98,10 +98,10 @@ app.post(`/form`, async (req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname), "client/build", "index.html");
+    res.sendFile(path.join(__dirname), "./client/build", "index.html");
   });
 }
 
