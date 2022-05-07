0e3b95c20fd6d1ef8c70d3703bab0cc72d99cb90
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-05-07 10:58:55 +0200

    trying working in production

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ const whitelist = [
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname), "client/build", "index.html");
+    res.sendFile(path.join(__dirname), "./client/build", "index.html");
   });
 }
 
