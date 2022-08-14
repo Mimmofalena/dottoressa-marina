729e063e51826ff955f25ddfaea46f380ed8d5c8
Domenico <dombusiness89@gmail.com>
2022-08-15 01:30:07 +0200

    Using SendGrid instead of nodemailer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const sgMail = require("@sendgrid/mail");
 
 const cors = require("cors");
 const nodemailer = require("nodemailer");
@@ -17,54 +18,90 @@ app.use(bodyParser.json());
 app.use(cors());
 app.post(`/form`, (req, res, next) => {
   let { message, firstName, lastName, email } = req.body;
-  const transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
 
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-    debug: true,
-    logger: true,
-  });
-
-  const mailOptions = {
-    from: process.env.MAIL_USER,
-    replyTo: email,
-    to: process.env.MAIL_TO,
-    subject: `Nuova mail ricevuta da ${email}`,
-    html: `<html>
- <body><div className="email" style="
-            border: 1px solid black;
-            padding: 20px;
-            font-family: sans-serif;
-            line-height: 2;
-            font-size: 20px;
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "cucinotta.dom@gmail.com", // Change to your recipient
+    from: "dombusiness89@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: `
+  <div className="email" style="
+             border: 1px solid black;
+             padding: 20px;
+             font-family: sans-serif;
+             line-height: 2;
+             font-size: 20px;
             ">
-            <h2>Hai ricevuto una mail da:</h2>
-            <p>Utente: ${firstName} ${lastName}</p>
-            <p>Email: ${email}</p>
-            <br/>
-            <p>Messaggio: <br/>${message}</p>
+             <h2>Hai ricevuto una mail da:</h2>
+             <p>Utente: ${firstName} ${lastName}</p>
+             <p>Email: ${email}</p>
+             <br/>
+             <p>Messaggio: <br/>${message}</p>
 
-            </div> </body>
-</html>`,
+             </div>`,
   };
-
-  transport.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.status(400);
-      return console.log(error);
-    }
-    console.log("Message sent:", info.messageId);
-  });
-
-  res.status(200);
-  res.end();
-  next();
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
+// app.post(`/form`, (req, res, next) => {
+//   let { message, firstName, lastName, email } = req.body;
+//   const transport = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+
+//     auth: {
+//       user: process.env.MAIL_USER,
+//       pass: process.env.MAIL_PASS,
+//     },
+//     debug: true,
+//     logger: true,
+//   });
+
+//   const mailOptions = {
+//     from: process.env.MAIL_USER,
+//     replyTo: email,
+//     to: process.env.MAIL_TO,
+//     subject: `Nuova mail ricevuta da ${email}`,
+//     html: `<html>
+//  <body><div className="email" style="
+//             border: 1px solid black;
+//             padding: 20px;
+//             font-family: sans-serif;
+//             line-height: 2;
+//             font-size: 20px;
+//             ">
+//             <h2>Hai ricevuto una mail da:</h2>
+//             <p>Utente: ${firstName} ${lastName}</p>
+//             <p>Email: ${email}</p>
+//             <br/>
+//             <p>Messaggio: <br/>${message}</p>
+
+//             </div> </body>
+// </html>`,
+//   };
+
+//   transport.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       res.status(400);
+//       return console.log(error);
+//     }
+//     console.log("Message sent:", info.messageId);
+//   });
+
+//   res.status(200);
+//   res.end();
+//   next();
+// });
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
